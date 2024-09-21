e8c05376fe5c499357f2c936dd27fa6b8e4ef6cd
huuphuoc <huuphuoc>
2024-09-21 23:04:13 +0700

    fix unexpected navigate to payment

--- Project 3/main.js
+++ Project 3/main.js
@@ -2,12 +2,12 @@ const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = menuBtn.querySelector("i");
 
-menuBtn.addEventListener("click", (e) => {
-  navLinks.classList.toggle("open");
+// menuBtn.addEventListener("click", (e) => {
+//   navLinks.classList.toggle("open");
 
-  const isOpen = navLinks.classList.contains("open");
-  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
-});
+//   const isOpen = navLinks.classList.contains("open");
+//   menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+// });
 
 navLinks.addEventListener("click", (e) => {
   navLinks.classList.remove("open");
